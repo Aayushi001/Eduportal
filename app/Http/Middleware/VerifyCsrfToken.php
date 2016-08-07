4dbf8b9d02943170e1858221f30c1bf3d9bf9f93
saxenarrohit <saxenarrohit@gmail.com>
2016-08-07 18:35:40 +0530

    Update VerifyCsrfToken.php
    stopped the csrf verification for the given urls

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends BaseVerifier
      * @var array
      */
     protected $except = [
-        //
+'/courses/level',"/courses/language","/courses/price","/courses/all"
     ];
 }
